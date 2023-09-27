808e56ca83b5d5090924603e3b04cc40d6f3d72b
CarterBloop <carter@tiogaventures.com>
2023-09-27 13:46:37 -0700

    Refactor block.go for optimization and readability.
    - Encapsulate VAO and VBO initialization into a separate method.
    - Add a function to define vertex attributes.
    - Segregate texture loading and setting its parameters.
    - Ensure to unbind buffers and arrays post initialization.
    - Ensure to close the texture file after reading.

--- block/block.go
+++ block/block.go
@@ -14,52 +14,68 @@ type Block struct {
 	Model   mgl32.Mat4
 	Texture uint32
 	Vao     uint32
+	Vbo     uint32
 }
 
 func NewBlock(pos mgl32.Vec3, texturePath string) (*Block, error) {
-	block := &Block{}
-	// set the model matrix to move the block to the specified position
-	block.Model = mgl32.Translate3D(pos[0], pos[1], pos[2])
-
-	// Load the texture
-	tex, err := newTexture(texturePath)
+	block := &Block{
+		Model: mgl32.Translate3D(pos[0], pos[1], pos[2]),
+	}
+	
+	// Load texture and handle error
+	tex, err := loadTexture(texturePath)
 	if err != nil {
 		return nil, err
 	}
 	block.Texture = tex
 
-	// Define the standard cube vertex data
-	vertices := CubeVertices
+	// Initialize VAO and VBO
+	if err := block.initVAOandVBO(); err != nil {
+		return nil, err
+	}
 
-	// Create VAO
+	return block, nil
+}
+
+func (b *Block) initVAOandVBO() error {
 	var vao, vbo uint32
+
 	gl.GenVertexArrays(1, &vao)
 	gl.GenBuffers(1, &vbo)
 
 	gl.BindVertexArray(vao)
-
 	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
-	gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4, gl.Ptr(vertices), gl.STATIC_DRAW)
 
+	gl.BufferData(gl.ARRAY_BUFFER, len(CubeVertices)*4, gl.Ptr(CubeVertices), gl.STATIC_DRAW)
+
+	// Define vertex attributes
+	defineVertexAttributes()
+
+	gl.BindBuffer(gl.ARRAY_BUFFER, 0) // Unbind buffer
+	gl.BindVertexArray(0)             // Unbind VAO
+
+	b.Vao = vao
+	b.Vbo = vbo
+
+	return nil
+}
+
+func defineVertexAttributes() {
 	// Position attribute
 	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 5*4, gl.PtrOffset(0))
 	gl.EnableVertexAttribArray(0)
 	// Texture coordinate attribute
 	gl.VertexAttribPointer(1, 2, gl.FLOAT, false, 5*4, gl.PtrOffset(3*4))
 	gl.EnableVertexAttribArray(1)
-
-	gl.BindVertexArray(0) // Unbind VAO
-
-	block.Vao = vao
-
-	return block, nil
 }
 
-func newTexture(file string) (uint32, error) {
+func loadTexture(file string) (uint32, error) {
 	imgFile, err := os.Open(file)
 	if err != nil {
 		return 0, fmt.Errorf("texture %q not found on disk: %v", file, err)
 	}
+	defer imgFile.Close()
+
 	img, _, err := image.Decode(imgFile)
 	if err != nil {
 		return 0, err
@@ -76,15 +92,9 @@ func newTexture(file string) (uint32, error) {
 	gl.ActiveTexture(gl.TEXTURE0)
 	gl.BindTexture(gl.TEXTURE_2D, texture)
 
-	// Nearest neighbor filtering
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+	// Set texture parameters
+	setTextureParameters()
 
-	// Texture wrapping/clamping
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-
-	// Load texture.
 	gl.TexImage2D(
 		gl.TEXTURE_2D,
 		0,
@@ -97,4 +107,14 @@ func newTexture(file string) (uint32, error) {
 		gl.Ptr(rgba.Pix))
 
 	return texture, nil
+}
+
+func setTextureParameters() {
+	// Nearest neighbor filtering for pixellated look
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+
+	// Texture wrapping/clamping
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
 }
